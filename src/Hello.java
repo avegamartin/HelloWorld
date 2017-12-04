469f6c969432003144e0fb42f21bb536b176399c
Antonio Vega <avegamartin@ono.com>
2017-12-04 20:41:32 +0100

    I have added my first Java class in the project.

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author antonio
+ *
+ */
+public class Hello {
+
+	/**
+	 * Implement the application
+	 * @param args arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
